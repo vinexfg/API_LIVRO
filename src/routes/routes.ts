daf6a6204b8d4188538e097a7821c1a5cf81fee2
Rodrigo <2rodrigosilva2@gmail.com>
2026-06-04 15:24:29 -0300

    Update routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,10 @@ import type { FastifyInstance } from "fastify";
 
 export default function routes(app: FastifyInstance) {
   app.get("/teste", async (request, reply) => {
-    reply.send({ teste: "Testando" });
+    reply.send({ message: "Rota Inicial" });
+  });
+
+  app.get("/teste", async (request, reply) => {
+    reply.send({ message: "Rota de Teste" });
   });
 }
