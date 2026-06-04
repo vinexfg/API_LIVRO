820ffd99aa1e7dbd9ca7f1196302a84ed845fffa
Vinicius Silva Gonçalves de Almeida <136178252+vinexfg@users.noreply.github.com>
2026-06-04 14:58:43 -0300

    server.js

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,19 @@ import Fastify from "fastify";
 const app = Fastify({
   logger: true,
 });
+
+app.register(routes);
+
+async function start() {
+  try {
+    await app.listen({
+      port: 3000,
+      host: "0.0.0.0",
+    });
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+}
+
+start();
