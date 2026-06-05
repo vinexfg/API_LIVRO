6f6944e2cf549a1864d092572c1bb1fae454c8f4
Rodrigo <2rodrigosilva2@gmail.com>
2026-06-05 17:30:09 -0300

    add

--- src/controllers/livroController.ts
+++ src/controllers/livroController.ts
@@ -29,7 +29,7 @@ export async function listarLivros(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  return reply.send(livros_db_teste);
+  return reply.status(200).send(livros_db_teste);
 }
 
 export async function buscarLivroId(
@@ -38,7 +38,7 @@ export async function buscarLivroId(
 ) {
   const { id } = request.params as { id: string };
 
-  return reply.send(livros_db_teste[Number(id)]);
+  return reply.status(200).send(livros_db_teste[Number(id)]);
 }
 
 export async function cadastrarLivro(
