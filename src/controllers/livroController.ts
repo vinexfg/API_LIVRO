ec677f1822a5b333ab27ec348e04ae01b1f5d4d6
Rodrigo <2rodrigosilva2@gmail.com>
2026-06-05 21:56:24 -0300

    Aplicação do crud prisma basico

--- src/controllers/livroController.ts
+++ src/controllers/livroController.ts
@@ -1,36 +1,30 @@
-import type { FastifyRequest, FastifyReply, FastifyInstance } from "fastify";
-import { buscarLivros } from "../services/livroServices.js";
+import type { FastifyRequest, FastifyReply } from "fastify";
+import {
+  buscarLivros,
+  buscarLivroPorId,
+  criarLivro,
+  atualizarLivroPorId,
+  apagarLivro,
+} from "../services/livroServices.js";
 
 interface Livro {
   titulo: string;
-  descricao: string;
+  descricao?: string;
   autor: string;
-  // avaliacao: number;
-  // preco: number;
-  // ano_de_publicacao: number;
-  // marca: string;
-  // isbn: number;
-  // foto_path: string;
+  avaliacao?: number;
+  preco?: number;
+  ano_de_publicacao?: number;
+  marca?: string;
+  isbn?: string;
+  foto_path?: string;
 }
-
-const livros_db_teste: Livro[] = [
-  {
-    titulo: "Livro 01",
-    descricao: "Descrição 01",
-    autor: "Autor 01",
-  },
-  {
-    titulo: "Livro 02",
-    descricao: "Descrição 02",
-    autor: "Autor 02",
-  },
-];
 
 export async function listarLivros(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  return reply.status(200).send(livros_db_teste);
+  const livros = await buscarLivros();
+  return reply.status(200).send(livros);
 }
 
 export async function buscarLivroId(
@@ -38,8 +32,8 @@ export async function buscarLivroId(
   reply: FastifyReply,
 ) {
   const { id } = request.params as { id: string };
-
-  return reply.status(200).send(livros_db_teste[Number(id)]);
+  const livro = await buscarLivroPorId(id);
+  return reply.status(200).send(livro);
 }
 
 export async function cadastrarLivro(
@@ -47,9 +41,11 @@ export async function cadastrarLivro(
   reply: FastifyReply,
 ) {
   const livro = request.body;
-  livros_db_teste.push(livro);
+  const resultado = await criarLivro(livro.titulo, livro.autor);
 
-  return reply.status(201).send({ message: "Livro cadastrado com sucesso!" });
+  return reply
+    .status(201)
+    .send({ message: "Livro cadastrado com sucesso! " + resultado });
 }
 
 export async function atualizarLivro(
@@ -57,10 +53,11 @@ export async function atualizarLivro(
   reply: FastifyReply,
 ) {
   const { id } = request.params as { id: string };
-  const livro_atualizado = request.body;
-  livros_db_teste[Number(id)] = livro_atualizado;
-
-  return reply.status(200).send({ message: "Livro atualizado com sucesso!" });
+  const livro = request.body;
+  const resultado = await atualizarLivroPorId(id, livro.titulo, livro.autor);
+  return reply
+    .status(200)
+    .send({ message: "Livro atualizado com sucesso!" + resultado });
 }
 
 export async function deletarLivro(
@@ -68,7 +65,76 @@ export async function deletarLivro(
   reply: FastifyReply,
 ) {
   const { id } = request.params as { id: string };
-  livros_db_teste.splice(Number(id), 1);
+  const resultado = await apagarLivro(id);
 
-  return reply.status(200).send({ message: "Livro deletado com sucesso!" });
+  return reply
+    .status(200)
+    .send({ message: "Livro deletado com sucesso!" + resultado });
 }
+
+//  CODIGO USADO DE BASE PARA TESTAR OS CONTROLLERS E ROTAS
+
+// const livros_db_teste: Livro[] = [
+//   {
+//     titulo: "Livro 01",
+//     descricao: "Descrição 01",
+//     autor: "Autor 01",
+//   },
+//   {
+//     titulo: "Livro 02",
+//     descricao: "Descrição 02",
+//     autor: "Autor 02",
+//   },
+// ];
+
+// LISTAR LIVROS DE TESTE - SEM PRISMA
+// export async function listarLivros(
+//   request: FastifyRequest,
+//   reply: FastifyReply,
+// ) {
+//   return reply.status(200).send(livros_db_teste);
+// }
+
+// BUSCAR LIVRO POR ID DE TESTE - SEM PRISMA
+// export async function buscarLivroId(
+//   request: FastifyRequest,
+//   reply: FastifyReply,
+// ) {
+//   const { id } = request.params as { id: string };
+
+//   return reply.status(200).send(livros_db_teste[Number(id)]);
+// }
+
+// CADASTRAR LIVRO TESTE - SEM PRISMA
+// export async function cadastrarLivro(
+//   request: FastifyRequest<{ Body: Livro }>,
+//   reply: FastifyReply,
+// ) {
+//   const livro = request.body;
+//   livros_db_teste.push(livro);
+
+//   return reply.status(201).send({ message: "Livro cadastrado com sucesso!" });
+// }
+
+// ATUALIZAR LIVRO TESTE - SEM PRISMA
+// export async function atualizarLivro(
+//   request: FastifyRequest<{ Body: Livro }>,
+//   reply: FastifyReply,
+// ) {
+//   const { id } = request.params as { id: string };
+//   const livro_atualizado = request.body;
+//   livros_db_teste[Number(id)] = livro_atualizado;
+
+//   return reply.status(200).send({ message: "Livro atualizado com sucesso!" });
+// }
+
+// DELETAR LIVRO TESTE - SEM PRISMA
+// export async function deletarLivro(
+//   request: FastifyRequest,
+//   reply: FastifyReply,
+// ) {
+//   const { id } = request.params as { id: string };
+//   livros_db_teste.splice(Number(id), 1);
+
+//   return reply.status(200).send({ message: "Livro deletado com sucesso!" });
+// }
--- src/services/livroServices.ts
+++ src/services/livroServices.ts
@@ -1,5 +1,46 @@
 import prisma from "../prisma.js";
 
 export async function buscarLivros() {
-  return await prisma.livro.findMany();
+  return prisma.livro.findMany();
+}
+
+export async function buscarLivroPorId(id: string) {
+  return prisma.livro.findUnique({
+    where: {
+      id: id,
+    },
+  });
+}
+
+export async function criarLivro(titulo: string, autor: string) {
+  return prisma.livro.create({
+    data: {
+      titulo: titulo,
+      autor: autor,
+    },
+  });
+}
+
+export async function atualizarLivroPorId(
+  id: string,
+  titulo: string,
+  autor: string,
+) {
+  return prisma.livro.update({
+    where: {
+      id: id,
+    },
+    data: {
+      titulo: titulo,
+      autor: autor,
+    },
+  });
+}
+
+export async function apagarLivro(id: string) {
+  return prisma.livro.delete({
+    where: {
+      id: id,
+    },
+  });
 }
